af92d5b1343ae0dc312e8dfd01d88aadeb628ab3
Sudan08 <sudanshakya08@gmail.com>
2024-10-25 14:17:05 +0545

    color generate from hex code removed text for now

--- code.ts
+++ code.ts
@@ -20,47 +20,21 @@ figma.ui.onmessage = async (msg: { type: string; colors: string }) => {
   // your HTML page is to use an object with a "type" property like this.
   if (msg.type === "create-colors") {
     // This plugin creates rectangles on the screen.
-    try {
-      (async () => {
-        const text = figma.createText();
-        console.log("hrer");
-        // Move to (50, 50)
-        text.x = 50;
-        text.y = 50;
-
-        // Load the font in the text node before setting the characters
-        await figma.loadFontAsync({
-          family: "Inter",
-          style: "Regular",
-        });
-        text.characters = "Hello world!";
-
-        // Set bigger font size and red color
-        text.fontSize = 18;
-        text.fills = [{ type: "SOLID", color: { r: 1, g: 0, b: 0 } }];
-        return figma.closePlugin();
-      })();
-      const purifiedArray = purifyUserInput(msg.colors);
-      console.log("purifiedArray", purifiedArray);
-      const nodes: SceneNode[] = [];
-      for (let i = 0; i < purifiedArray.length; i++) {
-        const rect = figma.createRectangle();
-        rect.x = i * 150;
-        const rgbValue = hexToRGB(purifiedArray[i]);
-        rect.fills = [{ type: "SOLID", color: rgbValue }];
-        rect.cornerRadius = 8;
-        const text = await createText(purifiedArray[i]);
-        figma.group([text, rect], figma.currentPage);
-        figma.currentPage.appendChild(rect);
-        nodes.push(rect);
-      }
-      console.log("this ", nodes);
-      figma.group(nodes, figma.currentPage);
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-    } catch (error) {
-      console.error(error);
+    const purifiedArray = purifyUserInput(msg.colors);
+    const nodes: SceneNode[] = [];
+    for (let i = 0; i < purifiedArray.length; i++) {
+      const rect = figma.createRectangle();
+      rect.x = i * 150;
+      const rgbValue = hexToRGB(purifiedArray[i]);
+      rect.fills = [{ type: "SOLID", color: rgbValue }];
+      rect.cornerRadius = 8;
+      figma.currentPage.appendChild(rect);
+      nodes.push(rect);
     }
+    figma.group(nodes, figma.currentPage);
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+    figma.closePlugin();
 
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at th  e bottom of the screen.
@@ -93,17 +67,4 @@ function duplicateHexNumber(hex: string) {
     return hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
   }
   return hex;
-}
-
-async function createText(hexCode: string) {
-  const text = figma.createText();
-  text.fontName = { family: "Inter", style: "Regular" };
-  text.x = 50;
-  text.y = 50;
-
-  text.characters = hexCode;
-  text.fontSize = 16;
-  text.fills = [{ type: "SOLID", color: { r: 0, g: 0, b: 0 } }];
-  console.log("text", text);
-  return text;
 }
